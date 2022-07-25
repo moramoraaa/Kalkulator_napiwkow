c63ea7735499cdd070b2256cc669139cfac42781
Kamila <morawskakamila@gmail.com>
2022-07-25 09:54:24 +0200

    zmiana z int na float, zmiana formy komentarza

--- Kalkulator_napiwkow.py
+++ Kalkulator_napiwkow.py
@@ -1,8 +1,8 @@
 # Kalkulator napiwków
 
-# Napisz program Kalkulator napiwku, w którym użytkownik wprowadza sumę 
-# gólną z rachunku wystawionego przez restaurację. Program powinien potem
-# wyświetlić dwie kwoty napiwku — w wysokości 15 i 20 procent.
+''' Napisz program Kalkulator napiwku, w którym użytkownik wprowadza sumę
+ogólną z rachunku wystawionego przez restaurację. Program powinien potem
+wyświetlić dwie kwoty napiwku — w wysokości 15 i 20 procent.'''
 
 def fajnakelnerka(x):
     return x * 0.2
@@ -12,7 +12,7 @@ def brzydkakelnerka(y):
 
 print("Cześć! Nie wiesz ile zostawić napiwku w restauracji. Spróbuj tego programu!")
 
-cena = int(input("Ile zapłaciłeś za swój obiad w restuarcji? Podaj kwotę bez wpisywania zł! "))
+cena = float(input("Ile zapłaciłeś za swój obiad w restuarcji? Podaj kwotę bez wpisywania zł! "))
 
 if cena == 0:
     print("Za darmo to raczej nie zjadłeś... Spróbuj jeszcze raz.")
@@ -24,7 +24,7 @@ else:
     except:
         print("Coś innego poszło nie tak... Spróbuj jeszcze raz.")
 
-    print("Jeżeli kelnerka jest ładna to daj jej", fajnakelnerka(cena), "zł")
-    print("Jeżeli kelnerka jest brzydka to daj jej", brzydkakelnerka(cena), "zł")
+    print("Jeżeli kelnerka jest ładna to daj jej", round(fajnakelnerka(cena), 2), "zł")
+    print("Jeżeli kelnerka jest brzydka to daj jej", round(brzydkakelnerka(cena), 2), "zł")
 
     print("Dziękuje za skorzystanie z programu. Z fartem!")
